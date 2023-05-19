063ad97ffef723bd706b7c12b377dfd2b2c68003
kathiriya18 <131762299+kathiriya18@users.noreply.github.com>
2023-05-19 15:34:31 +0530

    Add files via upload

---
+++ p19.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+
+main()
+{
+	int k,h,a;
+	for(k=5;k>=1;k--)
+	{
+		for(h=1;h<=k;h++)
+		{
+			printf("%d",h);
+		}
+		for(a=5;a>k;a--)
+		{
+			printf("  ");
+		}
+		for(h=k;h>=1;h--)
+		{
+			printf("%d",h);
+		}
+		printf("\n");
+    }
+	for(k=2;k<=5;k++)
+	{
+         for(h=1;h<=k;h++)
+		 {
+		 	printf("%d",h);
+		 }
+		 for(a=5;a>k;a--)
+		 {
+		 	printf("  ");
+		 }
+		 for(h=k;h>=1;h--)
+		 {
+		 	printf("%d",h);
+		 }
+		 printf("\n");
+    }
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	 
+    
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
